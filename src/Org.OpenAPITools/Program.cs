71e8734de758b5d5f0412ddba015f21cb5c2cecc
JazBong <jazbong@example.com>
2024-10-26 11:21:03 +0300

    Laboratory 4

--- src/Org.OpenAPITools/Program.cs
+++ src/Org.OpenAPITools/Program.cs
@@ -1,33 +1,40 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Org.OpenAPITools
 {
-    /// <summary>
-    /// Program
-    /// </summary>
     public class Program
     {
-        /// <summary>
-        /// Main
-        /// </summary>
-        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("logs/myapp.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        /// <summary>
-        /// Create the host builder.
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns>IHostBuilder</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog() // Используем Serilog для логирования
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                   webBuilder.UseStartup<Startup>()
-                             .UseUrls("http://0.0.0.0:8080/");
+                    webBuilder.UseStartup<Startup>()
+                              .UseUrls("http://0.0.0.0:8080/");
                 });
     }
 }
